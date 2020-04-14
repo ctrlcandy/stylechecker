be18a8aa5b0409830b9327fa5a97af8a1d216250
ctrlcandy <alexandraleshanu@yandex.ru>
2020-04-15 00:43:10 +0300

    Добавлен пользовательский интерфейс

--- Program.cs
+++ Program.cs
@@ -1,192 +1,22 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using DocumentFormat.OpenXml.Packaging;
-using DocumentFormat.OpenXml.Wordprocessing;
-using Xceed.Words.NET;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
-
-namespace ConsoleApp2
+namespace stylechecker
 {
-    class Program
+    static class Program
     {
-        private static string DefaultFontName { get; set; }
-        private static int DefaultFontSize { get; set; }
-        public string Name { get; }
-
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
         static void Main()
         {
-            string nameOfDocx = Console.ReadLine();
-            if (File.Exists(nameOfDocx))
-            {
-                using (WordprocessingDocument docx = WordprocessingDocument.Open(nameOfDocx, true))
-                {
-                    DocDefaults defaults = docx.MainDocumentPart.StyleDefinitionsPart.Styles.DocDefaults;
-                    RunFonts runFont = defaults.RunPropertiesDefault.RunPropertiesBaseStyle.RunFonts;
-                    try
-                    {
-                        DefaultFontSize = Convert.ToInt32(defaults.RunPropertiesDefault.RunPropertiesBaseStyle.FontSize.Val.Value);
-                    }
-                    catch
-                    {
-                        DefaultFontSize = 0;
-                    }
-
-                    Fonts fontTable = docx.MainDocumentPart.FontTablePart.Fonts;
-                    docx.Close();
-
-                    using (var doc = DocX.Load(nameOfDocx))
-                    {
-                        uint line = 0;
-                        string text;
-                        HashSet<string> usingFonts = new HashSet<string>();
-                        HashSet<double?> usingSize = new HashSet<double?>();
-
-                        foreach (var p in doc.Paragraphs)
-                        {
-                            line++; // строка документа  
-
-                            if (p.Text.Length > 15)
-                            {
-                                text = p.Text.Remove(15).TrimStart(' ').TrimStart('\t').TrimStart('\n') + "...";
-                                if (text == "...")
-                                    continue;
-                            }
-                            else
-                            {
-                                text = p.Text.TrimStart(' ').TrimStart('\t').TrimStart('\n');
-                                if (text == "")
-                                    continue;
-                            }
-
-                            // получение шрифтов, работает сносно
-                            try
-                            {
-                                foreach (var magic in p.MagicText)
-                                {
-                                    if (magic.formatting.FontFamily.Name != "Times New Roman")
-                                    {
-                                        if (usingFonts.Contains(magic.formatting.FontFamily.Name) == false)
-                                        {
-                                            usingFonts.Add(magic.formatting.FontFamily.Name);
-                                            Console.WriteLine("Строка " + line + " (начинается со слов \"" + text + "\" )" +
-                                        " - используется неверный шрифт: " + magic.formatting.FontFamily.Name);
-                                        }
-                                    }
-                                    else if (magic.formatting.FontFamily == null & usingFonts.Contains("null") == false)
-                                    {
-                                        usingFonts.Add("null");
-
-                                        Console.WriteLine("Строка " + line + " (начинается со слов \"" + text + "\" )" +
-                                   " - предположительно используется неверный шрифт. Возможные варианты:");
-                                        foreach (var f in fontTable.ChildElements)
-                                        {
-                                            Font font = f as Font;
-                                            if (!font.Name.ToString().Contains("Times New Roman") & !font.Name.ToString().Contains("serif"))
-                                                Console.WriteLine("* " + font.Name);
-                                        }
-                                    }
-                                }
-                                usingFonts.Clear();
-                            }
-                            catch
-                            {
-                                Console.WriteLine("Строка " + line + " (начинается со слов \"" + text + "\" )" +
-                                    " - предположительно используется неверный шрифт. Возможные варианты:");
-                                foreach (var f in fontTable.ChildElements)
-                                {
-                                    Font font = f as Font;
-                                    if (!font.Name.ToString().Contains("Times New Roman") & !font.Name.ToString().Contains("serif"))
-                                        Console.WriteLine("* " + font.Name);
-                                }
-                            }
-
-                            //получение кегля, работает плюс-минус нормально
-                            try
-                            {
-                                foreach (var magic in p.MagicText)
-                                {
-                                    if (magic.formatting.Size == null & magic.formatting.Size == 0 & usingSize.Contains(0) == false)
-                                    {
-                                        usingSize.Add(0);
-                                        if (DefaultFontSize != 0)
-                                        {
-                                            Console.WriteLine($"Строка " + line + " (начинается со слов \"" + text + "\" )" +
-                                            " - предположительно используется неверный кегль. Возможно: {DefaultFontSize/2}");
-                                        } 
-                                        else
-                                        {
-                                            Console.WriteLine("Строка " + line + " (начинается со слов \"" + text + "\" )" +
-                                            " - предположительно используется неверный кегль. ");
-                                        }
-                                    }
-                                    else if (magic.formatting.Size != 14 & usingSize.Contains(magic.formatting.Size) == false)
-                                    {
-                                        usingSize.Add(magic.formatting.Size);
-                                        Console.WriteLine("Строка " + line + " (начинается со слов \"" + text + "\" )" +
-                                            " - используется неверный кегль: " + magic.formatting.Size);
-                                    }
-                                }
-                                usingSize.Clear();
-                            }
-                            catch {
-                                Console.WriteLine("Строка " + line + " (начинается со слов \"" + text + "\" )" +
-                                        " - предположительно используется неверный кегль. ");
-                            }
-
-                            //получение междустрочного интервала, в целом работает корректно
-                            try
-                            {
-                                if (p.LineSpacing != 18)
-                                {
-                                    Console.WriteLine("Строка " + line + " (начинается со слов \"" + text + "\" )" +
-                                        " - используется неверный междустрочный интервал: " + (p.LineSpacing / 12).ToString("0.00").Replace(',', '.') + " вместо 1.5 строки");
-                                }
-                            }
-                            catch (NullReferenceException)
-                            {
-                                Console.WriteLine("Строка " + line + " (начинается со слов \"" + text + "\" )" +
-                                    " - предположительно используется неверный междустрочный интервал.");
-                            }
-                            catch (InvalidOperationException)
-                            {
-                                Console.WriteLine("Строка " + line + " (начинается со слов \"" + text + "\" )" +
-                                    " - предположительно используется неверный междустрочный интервал.");
-                            }
-
-                            //получение информации о выравнивании, работает вроде как корректно 
-                            try
-                            {
-                                if (p.Alignment.ToString() != "both")
-                                {
-                                    if (p.Alignment.ToString() == "center")
-                                    {
-                                        Console.WriteLine("Строка " + line + " (начинается со слов \"" + text + "\" )" +
-                                            " - используется выравнивание по центру вместо выравнивания по ширине.");
-                                    }
-                                    else if (p.Alignment.ToString() == "left")
-                                    {
-                                        Console.WriteLine("Строка " + line + " (начинается со слов \"" + text + "\" )" +
-                                            " - используется выравнивание по левому краю вместо выравнивания по ширине.");
-                                    }
-                                    else if (p.Alignment.ToString() == "right")
-                                    {
-                                        Console.WriteLine("Строка " + line + " (начинается со слов \"" + text + "\" )" +
-                                            " - используется выравнивание по правому краю вместо выравнивания по ширине.");
-                                    }
-                                    else
-                                    {
-                                        Console.WriteLine("Строка " + line + " (начинается со слов \"" + text + "\" )" +
-                                            " - предположительно используется неверное выравнивание по ширине");
-                                    }
-                                }
-                            }
-                            catch { }
-                        }
-                    }
-                    Console.ReadKey();
-                }
-            }
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form());
         }
     }
 }
---
+++ Stylechecker.cs
@@ -0,0 +1,273 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Xceed.Words.NET;
+using Font = DocumentFormat.OpenXml.Wordprocessing.Font;
+
+namespace stylechecker
+{
+    public class Stylechecker
+    {
+        private int DefaultOpenXmlFontSize;
+        private string DefaultFont { get; set; } = string.Empty;
+        private int DefaultFontSize;
+        private double DefaultLineSpacing;
+        private string DefaultAlignment { get; set; } = string.Empty;
+
+        private Fonts fontTable;
+        public string ResultErrors { get; set; } = string.Empty;
+        public string ResultWarnings { get; set; } = string.Empty;
+
+        public Stylechecker(string Font, int FontSize, double LineSpacing, string Alignment)
+        {
+            DefaultFont = Font;
+            DefaultFontSize = FontSize;
+           
+            switch (Alignment)
+            {
+                case ("По центру"):
+                    DefaultAlignment = "center";
+                    break;
+                case ("По левому краю"):
+                    DefaultAlignment = "left";
+                    break;
+                case ("По правому краю"):
+                    DefaultAlignment = "right";
+                    break;
+                default:
+                    DefaultAlignment = "both";
+                    break;
+            }
+            DefaultLineSpacing = LineSpacing * 12;
+        }
+
+        public void GetFontInfo(uint line, string text, Xceed.Document.NET.Paragraph p)
+        {
+            HashSet<string> usingFonts = new HashSet<string>();
+            try
+            {
+                foreach (var magic in p.MagicText)
+                {
+                    if (magic.formatting.FontFamily.Name != DefaultFont)
+                    {
+                        if (usingFonts.Contains(magic.formatting.FontFamily.Name) == false)
+                        {
+                            usingFonts.Add(magic.formatting.FontFamily.Name);
+                            ResultErrors += "Строка " + line + " (начинается со слов \"" + text + "\" )" +
+                        " - используется неверный шрифт: " + magic.formatting.FontFamily.Name + '\n';
+                        }
+                    }
+                    else if (magic.formatting.FontFamily == null & usingFonts.Contains("null") == false)
+                    {
+                        usingFonts.Add("null");
+
+                        ResultWarnings += "Строка " + line + " (начинается со слов \"" + text + "\" )" +
+                   " - предположительно используется неверный шрифт. Возможные варианты:" + '\n';
+                        foreach (var f in fontTable.ChildElements)
+                        {
+                            Font font = f as Font;
+                            if (!font.Name.ToString().Contains(DefaultFont) & !font.Name.ToString().Contains("serif"))
+                                ResultWarnings += "* " + font.Name + '\n';
+                        }
+                    }
+                }
+                usingFonts.Clear();
+            }
+            catch
+            {
+                ResultWarnings += "Строка " + line + " (начинается со слов \"" + text + "\" )" +
+                    " - предположительно используется неверный шрифт. Возможные варианты:" + '\n';
+                foreach (var f in fontTable.ChildElements)
+                {
+                    Font font = f as Font;
+                    if (!font.Name.ToString().Contains(DefaultFont) & !font.Name.ToString().Contains("serif"))
+                        ResultWarnings += "* " + font.Name + '\n';
+                }
+            }
+        }
+
+        public void GetFontSizeInfo(uint line, string text, Xceed.Document.NET.Paragraph p)
+        {
+            HashSet<double?> usingSize = new HashSet<double?>();
+            try
+            {
+                foreach (var magic in p.MagicText)
+                {
+                    if ((magic.formatting.Size == null || magic.formatting.Size == 0) & (usingSize.Contains(null) == false || usingSize.Contains(0) == false))
+                    {
+                        usingSize.Add(0);
+                        if (DefaultFontSize != 0 & DefaultOpenXmlFontSize != 0)
+                        {
+                            ResultWarnings += "Строка " + line + " (начинается со слов \"" + text + "\" )" +
+                            $" - предположительно используется неверный кегль. Возможно: {DefaultOpenXmlFontSize/2}" + '\n';
+                        }
+                        else
+                        {
+                            ResultWarnings += "Строка " + line + " (начинается со слов \"" + text + "\" )" +
+                            " - предположительно используется неверный кегль. " + '\n';
+                        }
+                    }
+                    else if (magic.formatting.Size != DefaultFontSize & usingSize.Contains(magic.formatting.Size) == false)
+                    {
+                        usingSize.Add(magic.formatting.Size);
+                        ResultErrors += "Строка " + line + " (начинается со слов \"" + text + "\" )" +
+                            " - используется неверный кегль: " + magic.formatting.Size + '\n';
+                    }
+                }
+                usingSize.Clear();
+            }
+            catch
+            {
+                ResultWarnings += "Строка " + line + " (начинается со слов \"" + text + "\" )" +
+                        " - предположительно используется неверный кегль. " + '\n';
+            }
+        }
+
+        public void GetLineSpacingInfo(uint line, string text, Xceed.Document.NET.Paragraph p)
+        {
+            try
+            {
+                if (p.LineSpacing == 0)
+                {
+                    throw new Exception();
+                }
+                else if (p.LineSpacing!= DefaultLineSpacing)
+                {
+                    ResultErrors += "Строка " + line + " (начинается со слов \"" + text + "\" )" +
+                        " - используется неверный междустрочный интервал: " +
+                        (p.LineSpacing / 12).ToString("0.00").Replace(',', '.') + $" вместо {(DefaultLineSpacing / 12).ToString("0.00").Replace(',', '.')} строки" + '\n';
+                }
+            }
+            catch
+            {
+                ResultWarnings += "Строка " + line + " (начинается со слов \"" + text + "\" )" +
+                    " - предположительно используется неверный междустрочный интервал." + '\n';
+            }
+        }
+
+        public void GetAlignmentInfo(uint line, string text, Xceed.Document.NET.Paragraph p)
+        {
+            try
+            {
+                if (p.Alignment.ToString() != DefaultAlignment)
+                {
+                    if (p.Alignment.ToString() == "center")
+                    {
+                        ResultErrors += "Строка " + line + " (начинается со слов \"" + text + "\" )" +
+                            " - используется выравнивание по центру." + '\n';
+                    }
+                    else if (p.Alignment.ToString() == "left")
+                    {
+                        ResultErrors += "Строка " + line + " (начинается со слов \"" + text + "\" )" +
+                            " - используется выравнивание по левому краю." + '\n';
+                    }
+                    else if (p.Alignment.ToString() == "right")
+                    {
+                        ResultErrors += "Строка " + line + " (начинается со слов \"" + text + "\" )" +
+                            " - используется выравнивание по правому краю." + '\n';
+                    }
+                    else if (p.Alignment.ToString() == "both")
+                    {
+                        ResultErrors += "Строка " + line + " (начинается со слов \"" + text + "\" )" +
+                            " - используется выравнивание по ширине." + '\n';
+                    }
+                    else
+                    {
+                        ResultWarnings += "Строка " + line + " (начинается со слов \"" + text + "\" )" +
+                            " - предположительно используется неверное выравнивание." + '\n';
+                    }
+                }
+            }
+            catch { }
+        }
+
+
+        void SetDefaults(string filePath)
+        {
+        
+                using (WordprocessingDocument docx = WordprocessingDocument.Open(filePath, true))
+                {
+                    DocDefaults defaults = docx.MainDocumentPart.StyleDefinitionsPart.Styles.DocDefaults;
+                    RunFonts runFont = defaults.RunPropertiesDefault.RunPropertiesBaseStyle.RunFonts;
+                    try
+                    {
+                        DefaultOpenXmlFontSize = Convert.ToInt32(defaults.RunPropertiesDefault.RunPropertiesBaseStyle.FontSize.Val.Value);
+                    }
+                    catch
+                    {
+                        DefaultOpenXmlFontSize = 0;
+                    }
+
+                    fontTable = docx.MainDocumentPart.FontTablePart.Fonts;
+                    docx.Close();
+                }
+            
+        }
+
+        public void MyDocument(string filePath, bool checkFonts = true, bool checkFontSize = true,
+            bool checkLineSpacing = true, bool checkAlignment = true)
+        {
+            try {
+                SetDefaults(filePath);
+                if (File.Exists(filePath))
+                {
+                    using (var doc = DocX.Load(filePath))
+                    {
+                        uint line = 0;
+                        string text;
+
+                        foreach (var p in doc.Paragraphs)
+                        {
+                            line++;  
+
+                            if (p.Text.Length > 15)
+                            {
+                                text = p.Text.Remove(15).TrimStart(' ').TrimStart('\t').TrimStart('\n') + "...";
+                                if (text == "...")
+                                    continue;
+                            }
+                            else
+                            {
+                                text = p.Text.TrimStart(' ').TrimStart('\t').TrimStart('\n');
+                                if (text == "")
+                                    continue;
+                            }
+
+                            if (checkFonts)
+                            {
+                                GetFontInfo(line, text, p);
+                            }
+                            if (checkFontSize)
+                            {
+                                GetFontSizeInfo(line, text, p);
+                            }
+                            if (checkLineSpacing)
+                            {
+                                GetLineSpacingInfo(line, text, p);
+                            }
+                            if (checkAlignment)
+                            {
+                                GetAlignmentInfo(line, text, p);
+                            }
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                ResultErrors = ResultWarnings = "Выбран некорректный тип файла!" + '\n' +
+                    "Убедитесь, что выбран файл формата DOCX. " +
+                    "При необоходимости откройте Microsoft Word и пересохраните Ваш документ в нужном формате.";
+            }
+        }
+    }
+}
